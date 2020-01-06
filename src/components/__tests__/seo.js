f4896d36c865b43ff43ea34ee4569a497bdabf07
misucai <52446572+misucai@users.noreply.github.com>
2020-01-06 10:05:15 +0800

    Update seo.js

--- src/components/__tests__/seo.js
+++ src/components/__tests__/seo.js
@@ -8,10 +8,10 @@ beforeEach(() => {
   useStaticQuery.mockImplementationOnce(() => ({
     site: {
       siteMetadata: {
-        title: "Ravan Scafi",
-        description: "Ravan Scafi's personal website.",
-        author: "Ravan Scafi",
-        siteUrl: "https://ravan.me",
+        title: "Corgi Awesome",
+        description: "Corgi Dog Breed Information.",
+        author: "corgiawesome",
+        siteUrl: "https://corgiawesome.com",
       },
     },
     image: {
